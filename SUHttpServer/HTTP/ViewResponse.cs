0e0f28741b525034e3270fd81b91b1db03fb09e6
albozhinov <a.bozhinov90@gmail.com>
2022-02-04 19:17:59 +0200

    Modified ViewResponse class. Added new Response body logic.

--- SUHttpServer/HTTP/ViewResponse.cs
+++ SUHttpServer/HTTP/ViewResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 
 namespace SUHttpServer.HTTP
 {
@@ -7,7 +8,7 @@ namespace SUHttpServer.HTTP
         private const char PathSeparator = '/';
 
 
-        public ViewResponse(string viewName, string controllerName)
+        public ViewResponse(string viewName, string controllerName, object model = null)
             : base("", ContentType.Html)
         {
             if (!viewName.Contains(PathSeparator))
@@ -21,7 +22,34 @@ namespace SUHttpServer.HTTP
 
             var viewContent = File.ReadAllText(viewPath);
 
+            if (model != null)
+            {
+                viewContent = PopulateModel(viewContent, model);
+            }
+            
             Body = viewContent;
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                        .GetType()
+                        .GetProperties()
+                        .Select(p => new
+                        {
+                            p.Name,
+                            Value = p.GetValue(model)
+                        });
+
+            foreach (var entry in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+
+                viewContent = viewContent.Replace($"{openingBrackets}{entry.Name}{closingBrackets}", entry.Value.ToString());
+            }
+
+            return viewContent;
         }
     }
 }
