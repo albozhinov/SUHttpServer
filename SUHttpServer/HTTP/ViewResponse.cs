ea66edffca0499118287802a9a2192b0c3badd01
albozhinov <a.bozhinov90@gmail.com>
2022-02-04 18:10:14 +0200

    Implemented ViewResponse logic.

---
+++ SUHttpServer/HTTP/ViewResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace SUHttpServer.HTTP
+{
+    public class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+
+
+        public ViewResponse(string viewName, string controllerName)
+            : base("", ContentType.Html)
+        {
+            if (!viewName.Contains(PathSeparator))
+            {
+                viewName = controllerName + PathSeparator + viewName;
+            }
+            var viewPath = Path.GetFullPath(
+                $"./Views/" 
+                + viewName.TrimStart(PathSeparator) 
+                + ".cshtml");
+
+            var viewContent = File.ReadAllText(viewPath);
+
+            this.Body = viewContent;
+        }
+    }
+}
